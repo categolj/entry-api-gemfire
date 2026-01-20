a762c961b20fa2c39cd1917b641675e49d96db03
Toshiaki Maki <makingx@gmail.com>
2026-01-20 17:08:53 +0900

    Remove siteAdmin property

--- src/main/java/am/ik/blog/github/Committer.java
+++ src/main/java/am/ik/blog/github/Committer.java
@@ -2,6 +2,5 @@ package am.ik.blog.github;
 
 public record Committer(String login, String id, String avatarUrl, String gravaterId, String url, String htmlUrl,
 		String followersUrl, String followingUrl, String gistsUrl, String starredUrl, String subscriptionsUrl,
-		String organizationsUrl, String reposUrl, String eventsUrl, String receivedEventsUrl, String type,
-		boolean siteAdmin) {
+		String organizationsUrl, String reposUrl, String eventsUrl, String receivedEventsUrl, String type) {
 }
